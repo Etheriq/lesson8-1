12f25edebc7f58ae34c37ea924df6bbe824237f3
Yuriy Tarnavskiy <xeon@Mac-mini-Yuriy.local>
2015-12-19 14:17:44 +0200

    in progress task 2

--- task2/task2/task2/ViewController.m
+++ task2/task2/task2/ViewController.m
@@ -10,18 +10,98 @@
 
 @interface ViewController ()
 
+@property(strong, nonatomic) NSArray *allFonts;
+
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self loadSystemFonts];
+    
 }
 
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)AddButtonAction:(UIBarButtonItem *)sender {
+    
+    NSAttributedString *origin = self.textLabel.attributedText;
+    
+    NSString *randomString = [self getRandomString];
+    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:randomString attributes:
+                                       @{
+                                         NSFontAttributeName: [UIFont fontWithName:[self getRandomSystemFont] size:[self getRandomStingSize]],
+                                         NSForegroundColorAttributeName: [self getRandomColor]
+                                         }];
+    
+    NSMutableParagraphStyle *paragraph = [NSMutableParagraphStyle new];
+    paragraph.paragraphSpacing = [self getRandomStingSize];
+    paragraph.headIndent = [self getRandomStingSize];
+    paragraph.firstLineHeadIndent = [self getRandomStingSize];
+    paragraph.tailIndent = [self getRandomStingSize];
+    
+    [attr addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, [randomString length])];
+    //    [attr appendAttributedString:origin];
+    [attr replaceCharactersInRange:NSMakeRange([origin length], 0) withString:@"\n"];
+    
+    self.textLabel.attributedText = attr;
+}
+
+- (IBAction)ClearButtonAction:(UIBarButtonItem *)sender {
+}
+
+#pragma mark - Custom methods
+
+- (void) loadSystemFonts {
+    NSMutableArray *fonts = [NSMutableArray array];
+    for (NSString *fontFamily in [UIFont familyNames]) {
+        for (NSString *fontName in [UIFont fontNamesForFamilyName:fontFamily]) {
+            [fonts addObject:fontName];
+        }
+    }
+    
+    self.allFonts = [NSArray arrayWithArray:fonts];
+}
+
+- (NSString *) getRandomString {
+    
+    NSString *baseString = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut pulvinar urna. Ut dignissim interdum risus eu lacinia. Phasellus congue justo lectus, id imperdiet odio euismod in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec laoreet ligula tincidunt aliquam luctus. Aliquam fringilla urna nec dui lacinia sodales. Proin in est id dolor vestibulum cursus. Ut orci neque, varius nec arcu nec, laoreet vulputate augue. Phasellus eu libero massa. Curabitur vel porta nulla, in sollicitudin tellus. Praesent sed quam velit. Nunc ut feugiat nunc, eu molestie odio. Praesent lacinia massa metus, dapibus scelerisque dolor hendrerit ac. Quisque et lorem condimentum, sodales sem vitae, condimentum ex. Phasellus venenatis, erat a tincidunt faucibus, metus tortor semper nisi, id mattis urna magna vel diam. Ut elementum ante id felis laoreet, sed vehicula est tristique. Aliquam semper lacinia ornare. Pellentesque urna tellus, accumsan non ipsum ac, cursus dictum erat. Nam sit amet eros egestas elit tempor tempor.";
+    
+    NSUInteger baseStringLenght = [baseString length];
+    NSUInteger from = arc4random() % baseStringLenght;
+    NSUInteger to = arc4random() % (baseStringLenght - from);
+    
+    return [baseString substringWithRange:NSMakeRange(from, to)];
+}
+
+- (NSString*) getRandomSystemFont {
+    
+    NSUInteger randIndex = arc4random() % [self.allFonts count];
+    
+    return [self.allFonts objectAtIndex:randIndex];
+}
+
+- (NSUInteger) getRandomStingSize {
+    
+    return (int)arc4random() % 20 + 1;
+}
+
+- (UIColor *) getRandomColor {
+    float r = ((arc4random() % 256) / 255.f);
+    float g = ((arc4random() % 256) / 255.f);
+    float b = ((arc4random() % 256) / 255.f);
+    
+    return [[UIColor alloc] initWithRed:r green:g blue:b alpha:1.f];
 }
 
 @end
