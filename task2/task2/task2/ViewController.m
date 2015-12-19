d54499022a62ab258f93890fa9d3b6cd7e964add
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2015-12-20 00:35:56 +0200

    in progress

--- task2/task2/task2/ViewController.m
+++ task2/task2/task2/ViewController.m
@@ -30,41 +30,52 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+//    self.cur
+    
+//    self.textLabel.bounds.size.width = self.view.bounds.size.width;
+}
+
 #pragma mark - Actions
 
 - (IBAction)AddButtonAction:(UIBarButtonItem *)sender {
     
-    NSAttributedString *origin = self.textLabel.attributedText;
-    
     NSString *randomString = [self getRandomString];
-    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithString:randomString attributes:
+    NSMutableAttributedString *content = [[NSMutableAttributedString alloc] initWithString:randomString attributes:
                                        @{
-                                         NSFontAttributeName: [UIFont fontWithName:[self getRandomSystemFont] size:[self getRandomStingSize]],
+                                         NSFontAttributeName: [UIFont fontWithName:[self getRandomSystemFont] size:[self getRandomIntFrom:10 andTo:25]],
                                          NSForegroundColorAttributeName: [self getRandomColor]
                                          }];
     
-    NSMutableParagraphStyle *paragraph = [NSMutableParagraphStyle new];
-    paragraph.paragraphSpacing = 5;// [self getRandomStingSize];
-    paragraph.headIndent = 5;// [self getRandomStingSize];
-    paragraph.firstLineHeadIndent = 5; // [self getRandomStingSize];
-    paragraph.tailIndent = 5; //[self getRandomStingSize];
-//    paragraph.lineBreakMode = NSLineBreakByWordWrapping;
+    NSMutableParagraphStyle *paragraph = [[NSMutableParagraphStyle alloc] init];
     
-    [attr addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, [randomString length])];
-        [attr appendAttributedString:origin];
-    [attr replaceCharactersInRange:NSMakeRange([origin length], 0) withString:@"\n"];
-    
-    self.textLabel.attributedText = attr;
+    paragraph.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraph.paragraphSpacing = [self getRandomIntFrom:5 andTo:15];
+    paragraph.lineSpacing = [self getRandomIntFrom:5 andTo:15];
+    paragraph.alignment = [self getRandomIntFrom:0 andTo:3];
 
-//        self.textLabel.text = attr.string;
+    [content addAttribute:NSParagraphStyleAttributeName value:paragraph range:NSMakeRange(0, 1)];
+    [content insertAttributedString:[[NSAttributedString alloc] initWithString:@"\n"] atIndex:0];
+    NSMutableAttributedString *origin = [[NSMutableAttributedString alloc] initWithAttributedString:self.textLabel.attributedText];
+    [origin appendAttributedString:content];
+    
+    self.textLabel.attributedText = origin;
     
     NSLog(@"%@", randomString);
 }
 
 - (IBAction)ClearButtonAction:(UIBarButtonItem *)sender {
+    self.textLabel.text = @"";
 }
 
 #pragma mark - Custom methods
+
+- (NSInteger) getRandomIntFrom:(NSInteger) from andTo:(NSInteger) to {
+    
+    return from + arc4random() % (to + 1 - from);
+}
 
 - (void) loadSystemFonts {
     NSMutableArray *fonts = [NSMutableArray array];
@@ -82,14 +93,12 @@
     NSString *baseString = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut pulvinar urna. Ut dignissim interdum risus eu lacinia. Phasellus congue justo lectus, id imperdiet odio euismod in. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec laoreet ligula tincidunt aliquam luctus. Aliquam fringilla urna nec dui lacinia sodales. Proin in est id dolor vestibulum cursus. Ut orci neque, varius nec arcu nec, laoreet vulputate augue. Phasellus eu libero massa. Curabitur vel porta nulla, in sollicitudin tellus. Praesent sed quam velit. Nunc ut feugiat nunc, eu molestie odio. Praesent lacinia massa metus, dapibus scelerisque dolor hendrerit ac. Quisque et lorem condimentum, sodales sem vitae, condimentum ex. Phasellus venenatis, erat a tincidunt faucibus, metus tortor semper nisi, id mattis urna magna vel diam. Ut elementum ante id felis laoreet, sed vehicula est tristique. Aliquam semper lacinia ornare. Pellentesque urna tellus, accumsan non ipsum ac, cursus dictum erat. Nam sit amet eros egestas elit tempor tempor.";
     
     NSUInteger baseStringLenght = [baseString length];
-    NSUInteger from = arc4random() % baseStringLenght;
-    NSUInteger to = arc4random() % (baseStringLenght - from);
+    NSUInteger from = [self getRandomIntFrom:0 andTo:baseStringLenght];
+    NSUInteger to = baseStringLenght - [self getRandomIntFrom:from andTo:baseStringLenght] - 1;
     
-    NSLog(@"Len = %li, from %li to %li", baseStringLenght, from, to);
-
-    return [baseString substringFromIndex:from];
-    
-//    return [baseString substringWithRange:NSMakeRange(from, to)];
+//    NSLog(@"Len = %li, from %li to %li", baseStringLenght, from, to);
+  
+    return [baseString substringWithRange:NSMakeRange(from, to)];
 }
 
 - (NSString*) getRandomSystemFont {
